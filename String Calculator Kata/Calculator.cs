99904da7b44ac2f85aeaab3be524f46f1170c9f1
Brandon Born <brandon.born@gmail.com>
2015-01-13 23:10:33 -0800

    Initial Commit

---
+++ String Calculator Kata/Calculator.cs
@@ -0,0 +1,94 @@
+﻿// Kata Taken From http://osherove.com/tdd-kata-1/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace String_Calculator_Kata
+{
+    /// <summary>
+    /// The SUT used for this kata
+    /// </summary>
+    public class Calculator
+    {
+
+        /// <summary>
+        /// The method can take 0, 1 or 2 numbers, and will return their sum
+        /// </summary>
+        /// <param name="numbers">String of numbers</param>
+        /// <returns>sum</returns>
+        public static int Add(string numbers)
+        {
+            // Stores all negative numbers found for error check
+            List<int> negativeNumbers = new List<int>();
+            // Used to store summation result
+            int result = 0;
+
+            // We know it's an empty string so we're done.
+            if (!string.IsNullOrEmpty(numbers))
+            {
+                // If the string contains a comma we know we need to add them together
+                // also detects if //;\n pattern may exist
+                if (numbers.Contains(",") || numbers.Contains("\n"))
+                {
+                    // base delimiters
+                    List<char> delimters = new List<char>() { ',', '\n' };
+
+                    //add new delimiter if exists
+                    if(numbers.StartsWith("//") && numbers.Length > 3 && numbers[3].Equals('\n'))
+                    {
+                        delimters.Add(numbers[2]);
+                    }
+
+                    foreach (string number in numbers.Split(delimters.ToArray()))
+                    {
+                        result += ParseInt(number, negativeNumbers);
+                    }
+                }
+                else
+                {
+                    // Only one number so parse it and return if possible
+                    result = ParseInt(numbers, negativeNumbers);
+                }
+            }
+
+            //check for negative numbers
+            if (negativeNumbers.Count() > 0)
+            {
+                string exceptionMessage = "negatives not allowed: ";
+
+                foreach(int negative in negativeNumbers)
+                {
+                    exceptionMessage += negative + ",";
+                }
+
+                throw new ArgumentException(exceptionMessage.TrimEnd(','));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Does the parsing heavy lifting and error checking
+        /// </summary>
+        /// <param name="numberToParse">the string that will be parsed</param>
+        /// <param name="negativeNumbers">List to add to if a negative is found</param>
+        /// <returns></returns>
+        private static int ParseInt(string numberToParse, List<int> negativeNumbers)
+        {
+            int output = 0;
+
+            int.TryParse(numberToParse, out output);
+
+            if (output < 0)
+            {
+                negativeNumbers.Add(output);
+            }
+            
+            return output;
+        }
+    }
+}
---
+++ String Calculator Unit Test/CalculatorTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using String_Calculator_Kata;
+
+namespace String_Calculator_Unit_Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddEmptyString()
+        {
+            int result = Calculator.Add(string.Empty);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void AddOneNumberString()
+        {
+            int result = Calculator.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void AddNoNumberWithDelimiterString()
+        {
+            int result = Calculator.Add(",");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void AddNoNumberWithIncorrectDelimiterString()
+        {
+            int result = Calculator.Add("|");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void AddTwoNumberString()
+        {
+            int result = Calculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddThreeNumberString()
+        {
+            int result = Calculator.Add("1,2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void AddThreeNumberWithNewlineString()
+        {
+            int result = Calculator.Add("1\n2\n3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void AddThreeNumberWithNewlineMixedString()
+        {
+            int result = Calculator.Add("1\n2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void AddOneNumberWithNewlineMixedStrangeString()
+        {
+            int result = Calculator.Add("1,\n");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void AddTwoNumberWithNewlineMixedStrangeString()
+        {
+            int result = Calculator.Add("1,\n3");
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void AddTwoNumberWithVariableDelimiterString()
+        {
+            int result = Calculator.Add("//;\n1;3");
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void AddTwoNumberWithVariableDelimiterTwoString()
+        {
+            int result = Calculator.Add("///\n1/3");
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void AddTwoNumberWithVariableDelimiterAndStandardString()
+        {
+            int result = Calculator.Add("//;\n1;3\n5,6");
+
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void SingleNegativeNumber()
+        {
+            string errorMessage = string.Empty;
+
+            try
+            {
+                Calculator.Add("-2");
+            }
+            catch (ArgumentException ex)
+            {
+                errorMessage = ex.Message;
+            }
+
+            Assert.AreEqual("negatives not allowed: -2", errorMessage);
+        }
+
+        [TestMethod]
+        public void MultipleNegativeNumbers()
+        {
+            string errorMessage = string.Empty;
+
+            try
+            {
+                Calculator.Add("-2,3\n-44");
+            }
+            catch (ArgumentException ex)
+            {
+                errorMessage = ex.Message;
+            }
+
+            Assert.AreEqual("negatives not allowed: -2,-44", errorMessage);
+        }
+    }
+}
